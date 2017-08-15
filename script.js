5fda74970f8ea31c510b0928874caa7d50699899
Al Imran <imranmmcbd@users.noreply.github.com>
2017-08-15 22:48:51 +0600

    New cal

---
+++ script.js
@@ -0,0 +1,23 @@
+var box = document.getElementById('display');
+function addtoscreen(x) {
+    box.value += x;
+    if (x === 'c') {
+        box.value = '';
+    }
+    
+    if (x === 'ce') {
+        box.value = '';
+    }
+}
+function answer() {
+    var x;
+    x = box.value;
+    x = eval(x);
+    box.value = x;
+}
+function backspace() {
+    var number = box.value;
+    var len = number.length - 1;
+    var newnumber = number.substring(0,len);
+    box.value = newnumber;
+}
